898da0507d7dd7c92ca30093d48654a75b9fa4dc
Ula <ptx54455@student.torun.merito.pl>
2026-05-31 09:54:41 +0200

    pierwsze

---
+++ test3105.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return '<h1>Witaj MERITO!</h1><p>Autor: Ula</p>'
+
+if __name__ == '__main__':
+    app.run(debug=True)
