187fef5ac978d1a3bedd76eae43fe3a04c5f6114
Ula <ptx54455@student.torun.merito.pl>
2026-05-31 09:56:32 +0200

    pierwsze

--- test3105.py
+++ test3105.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return '<h1>Witaj MERITO!</h1><p>Autor: Ula</p>'
+    return '<h1>Witaj MERITO!</h1><p>Autor: Ula </p>'
 
 if __name__ == '__main__':
     app.run(debug=True)
